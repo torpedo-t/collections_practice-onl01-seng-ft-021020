24b5632b5a0014392609770cc06d9da1c35979de
Thomas John Helin <thomashelin@yahoo.com>
2020-02-20 19:59:17 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,47 @@
+def sort_array_asc(array)
+  array.sort do |a,b|
+    a <=> b
+ end
+end
 
+def sort_array_desc(array)
+  array.sort do |a,b|
+    b <=> a 
+ end
+end
+
+def sort_array_char_count(array)
+  array.sort do |left,right| 
+  left.length <=> right.length
+ end
+end
+
+def swap_elements(array)
+  array[1], array[2] = array[2], array[1]
+array
+end
+
+def reverse_array(array)
+  array.reverse!
+end
+
+def kesha_maker(array)
+  array.each do |name|
+  name[2] = "$"   
+ end  
+end
+
+def find_a(array)
+  array.select { |word| word.start_with?('a', 'A') }
+end
+
+def sum_array(array)
+  array.inject(0) {|sum,x| sum + x }
+end
+
+def add_s(array)
+  array.each_with_index.map do |element, index|
+    index == 1 ? element : element + "s"
+  end
+end
+  
--- spec/collections_practice_spec.rb
+++ spec/collections_practice_spec.rb
@@ -1,3 +1,4 @@
+
 require 'spec_helper'
 
 describe 'collections practice' do
